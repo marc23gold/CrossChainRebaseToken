b70a7a5098a7d2a0e16a0f496c41d55d01cc040c
Marc <haitianhero6@gmail.com>
2025-03-02 08:22:59 -0500

    created getter function for the interest rate

--- src/RebaseToken.sol
+++ src/RebaseToken.sol
@@ -16,16 +16,44 @@ contract RebaseToken is ERC20 {
 
     error RebaseToken__InterestRateDecreaseError(uint256 newInterestRate, uint256 oldInterestRate);
 
+    event InterestRateSet(uint256 newInterestRate);
+
+    mapping (address user => uint256 interestRate) private s_userInterestRate;
+
     uint256 private s_interestRate = 5e10;
 
     constructor() ERC20("Rebase", "RBS") {}
 
+    /**
+     * @notice Sets the interest rate 
+     * @dev The interest rate can only decrease
+     * @param _newInterestRate The new interest rate
+     */
     function setInterestRate(uint256 _newInterestRate) external {
         //set the interest rate
         if( _newInterestRate < s_interestRate) {
             revert RebaseToken__InterestRateDecreaseError(_newInterestRate, s_interestRate);
         }
         s_interestRate = _newInterestRate;
+        emit InterestRateSet(_newInterestRate);
     }
+
+    function mintRebase(uint256 _amount, address _to) external{
+         _mint(_to, _amount);
+        s_userInterestRate[msg.sender] = s_interestRate;
+
+    }
+
+    function burnRebase(uint256 _amount, address _from) external{
+
+    }
+
+    /**
+     * @notice Gets the global interest rate
+     * @return The global interest rate
+     */
+    function getUserInterestRate(address _user) external view returns(uint256){
+        return s_userInterestRate[_user];
+    }   
 
 }
